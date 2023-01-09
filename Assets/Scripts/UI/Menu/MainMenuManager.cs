ebddcdb1ba17b71f47b8334fe4d8d7131b9f8841
VlC <victorstancioiu@gmail.com>
2023-01-10 00:04:31 +0100

    Revert "Some UI layout changes"
    This reverts commit 0adea06fe1cffcc1185c6481a21cbe267f13771f.

--- Assets/Scripts/UI/Menu/MainMenuManager.cs
+++ Assets/Scripts/UI/Menu/MainMenuManager.cs
@@ -534,7 +534,6 @@ public class MainMenuManager : MonoBehaviour, ILobbyCallbacks, IInRoomCallbacks,
         
         newRulePrompt.transform.Find("Image/LblCondition/ConditionDropdown").GetComponent<TMP_Dropdown>().AddOptions(POSSIBLE_CONDITIONS);
         newRulePrompt.transform.Find("Image/LblAction/ActionDropdown").GetComponent<TMP_Dropdown>().AddOptions(POSSIBLE_ACTIONS);
-        characterDropdown.captionText.text = $"<sprite={characterDropdown.value + 3}>";
 
         GlobalController.Instance.DiscordController.UpdateActivity();
         EventSystem.current.SetSelectedGameObject(title);
@@ -813,11 +812,6 @@ public class MainMenuManager : MonoBehaviour, ILobbyCallbacks, IInRoomCallbacks,
             newRulePromptAnimator.SetBool("open", newRulePrompt.activeSelf);
         
         EventSystem.current.SetSelectedGameObject(newRuleSelected);
-    }
-
-    public void OpenRNGRules()
-    {
-        
     }
     public void OpenOptions() {
         title.SetActive(false);
@@ -1386,10 +1380,7 @@ public class MainMenuManager : MonoBehaviour, ILobbyCallbacks, IInRoomCallbacks,
         sfx.PlayOneShot(Enums.Sounds.UI_WindowOpen.GetClip());
     }
 
-    public void SwapCharacter(TMP_Dropdown dropdown)
-    {
-        dropdown.captionText.text = $"<sprite={dropdown.value + 3}>";
-    
+    public void SwapCharacter(TMP_Dropdown dropdown) {
         Hashtable prop = new() {
             { Enums.NetPlayerProperties.Character, dropdown.value }
         };
