619629bbb587f2da925eac14a7e4aeef4b5febd9
VlC <victorstancioiu@gmail.com>
2023-04-20 19:18:13 +0200

    crap idk

--- Assets/Scripts/UI/Menu/SpecialRuleContainer.cs
+++ Assets/Scripts/UI/Menu/SpecialRuleContainer.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 public class SpecialRuleContainer : MonoBehaviour
 {
     public List<string> possibleSpecialRules;
+    public GameObject elementTemplate;
+    
     // Start is called before the first frame update
     void Start()
     {
