787fd769c6612a1f6d2286bf854b112fe855e447
vlcoo <victorstancioiu@gmail.com>
2023-06-09 04:04:56 +0200

    more prefabs for new chars

--- Assets/Scripts/UI/Menu/MainMenuManager.cs
+++ Assets/Scripts/UI/Menu/MainMenuManager.cs
@@ -191,7 +191,7 @@ public class MainMenuManager : MonoBehaviour, ILobbyCallbacks, IInRoomCallbacks,
     public void OnPlayerPropertiesUpdate(Player player, Hashtable playerProperties) {
         // increase or remove when toadette or another character is added
         Utils.GetCustomProperty(Enums.NetRoomProperties.Debug, out bool debug);
-        if (PhotonNetwork.IsMasterClient && Utils.GetCharacterIndex(player) > 6 && !debug)
+        if (PhotonNetwork.IsMasterClient && Utils.GetCharacterIndex(player) > 7 && !debug)
             SwapCharacterExplicit(0);
         UpdateSettingEnableStates();
     }
