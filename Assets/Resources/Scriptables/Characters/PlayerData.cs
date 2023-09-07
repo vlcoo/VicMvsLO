98161112da059895e024582d47673a54d64ac16d
VlC <victorstancioiu@gmail.com>
2023-09-07 14:22:14 +0200

    add discord "playing as" detail

--- Assets/Resources/Scriptables/Characters/PlayerData.cs
+++ Assets/Resources/Scriptables/Characters/PlayerData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData", order = 0)]
 public class PlayerData : ScriptableObject {
-    public string soundFolder, prefab, uistring;
+    public string soundFolder, prefab, uistring, legalName;
     public Sprite loadingSmallSprite, loadingBigSprite, readySprite, silhouetteSprite;
     public RuntimeAnimatorController smallOverrides, largeOverrides;
     public bool isBowsers;
