24adcb90d7a9ed7b0155183480ed5d977186711f
vic <victorstancioiu@gmail.com>
2024-08-19 23:20:38 +0200

    add 6 new characters (part 1)

--- Assets/Scripts/UI/Menu/Loading/LoadingLevelCreator.cs
+++ Assets/Scripts/UI/Menu/Loading/LoadingLevelCreator.cs
@@ -12,7 +12,7 @@ public class LoadingLevelCreator : MonoBehaviour
             return;
 
         if (GameManager.Instance.levelDesigner != "")
-            text.text = $"Level designed by: {GameManager.Instance.levelDesigner}";
+            text.text = $"Level designed by {GameManager.Instance.levelDesigner}";
         if (GameManager.Instance.MatchConditioner.count >= 8) readyText.text = "You better be ready.";
         enabled = false;
     }
