9e2136df079b0624888e4d6ddaee1db70687c522
vic <victorstancioiu@gmail.com>
2025-02-12 15:31:58 +0100

    change intro screen

---
+++ Assets/Scripts/UI/CompanyNameGenerator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CompanyNameGenerator : MonoBehaviour {
+    public string companyName;
+    [SerializeField] private TMP_Text label;
+
+    void Start() {
+        companyName = GenerateCompanyName();
+        label.text = $"{companyName}<space=-6><font=\"BiosFont\"><size=40><voffset=28>\u00ae";
+    }
+
+    private string GenerateCompanyName() {
+        var c = "N";
+        c += new[] {"i", "l"}[Random.Range(0, 2)];
+        c += new[] {"n", "m"}[Random.Range(0, 2)];
+        c += new[] {"i", "t", "l"}[Random.Range(0, 3)];
+        c += new[] {"e", "a", "o"}[Random.Range(0, 3)];
+        c += new[] {"n", "m"}[Random.Range(0, 2)];
+        c += c != "Ninten" ? "d" : "t";
+        c += new[] {"e", "a", "o"}[Random.Range(0, 3)];
+        return c;
+    }
+}
