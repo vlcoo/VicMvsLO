92037e5dea7cafafbef6051ec2eb02e9632a5455
Lust <104316481+ArianLust@users.noreply.github.com>
2026-03-28 17:18:06 +0100

    kek

--- Assets/Scripts/DiscordController.cs
+++ Assets/Scripts/DiscordController.cs
@@ -13,7 +13,7 @@ namespace NSMB {
 #pragma warning disable CS0162
 
         //---Static Variables
-        private static readonly long DiscordAppId = 962073502469459999;
+        private static readonly long DiscordAppId = 1485670444840390717;
 
         //---Private Variables
         private Discord.Discord discord;
